b2d26469b1e0e31d094d7c2ec686811a2701be01
Jacob Bollmann <jacob.bollmann@yahoo.com>
2023-12-30 21:58:46 -0600

    Client side get route working and rendering to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,8 @@
 console.log('client.js is sourced!');
+//Get route to render most recent calculation and all calc history []
+//Post route to send num values and operator []
+//Equal button when clicked sends post route
+//C button clears inputs
 
 function getCalculations() {
   console.log('In get calulations route:')
@@ -11,6 +15,22 @@ function getCalculations() {
     const mostRecentHistory = document.querySelector('#mostRecent');
     const allHistory = document.querySelector('#history');
     const calculations = response.data;
+    console.log(calculations);
+    mostRecentHistory.innerHTML = '';
+    allHistory.innerHTML = '';
+
+    for ( item of calculations ) {
+      console.log('Item loop', item);
+      console.log('calculations loop', calculations);
+
+      mostRecentHistory.innerHTML = `
+        <p>${item.result}</p>
+      `
+
+      allHistory.innerHTML += `
+    <p>${item.numOne} ${item.operator} ${item.numTwo} = ${item.result}</p>
+      `
+    }
   })
   .catch((error) => {
     console.log('Error', error);
