854521af9f3a1099d84e03c3fb49e615f2cab388
Jacob Bollmann <jacob.bollmann@yahoo.com>
2023-12-30 20:53:24 -0600

    All server side tests passing

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,21 @@
 console.log('client.js is sourced!');
+
+function getCalculations() {
+  console.log('In get calulations route:')
+
+  axios({
+    method: 'GET',
+    url: '/calculations',
+  })
+  .then((response) => {
+    const mostRecentHistory = document.querySelector('#mostRecent');
+    const allHistory = document.querySelector('#history');
+    const calculations = response.data;
+  })
+  .catch((error) => {
+    console.log('Error', error);
+  })
+};
+
+
+getCalculations();
--- server/server.js
+++ server/server.js
@@ -9,13 +9,49 @@ app.use(express.static('server/public'));
 // calculation objects:
 let calculations = []
 
+//create get route to send calculations [x]
+//create a post route to obtain values [x]
+//Post route should do math for operator used. +/-/*// [x]
+//After post route math. Result should be stored and added to object then pushed to calculations [x]
 
 // Here's a wonderful place to make some routes:
 
 // GET /calculations
+app.get('/calculations', (req, res) => {
+  console.log('In Get Route:')
+  res.send(calculations);
+});
 
 // POST /calculations
+app.post('/calculations', (req, res) => {
+  const newCalculation = req.body;
+  const numOne = parseInt(req.body.numOne);
+  const numTwo = parseInt(req.body.numTwo);
+  const operator = req.body.operator;
+//  console.log('New Calc:', newCalculation);
+ // console.log('Operator:', operator);
+ // console.log('Num One:', numOne);
+ // console.log('Num Two:', numTwo);
 
+  let result = null;
+  if ( operator === '+' ) {
+    result = numOne + numTwo
+  } else if ( operator === '-' ) {
+    result = numOne - numTwo
+  } else if ( operator === '*' ) {
+    result = numOne * numTwo
+  } else if ( operator === '/' ) {
+    result = numOne / numTwo
+  } else {
+    res.status(400).send('Error with calculation');
+    return
+  };
+  //console.log(result);
+
+  newCalculation.result = result;
+  calculations.push(newCalculation);
+  res.status(201).send('Post route successful');
+});
 
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
 // 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
